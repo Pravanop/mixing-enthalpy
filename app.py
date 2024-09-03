872f4b96dbe49a6ddb8480b2daed31898b118e0d
Pravan Omprakash <pravanomprakash.181mt035@nitk.edu.in>
2024-09-03 09:17:16 -0500

    updating app

--- app.py
+++ app.py
@@ -162,6 +162,8 @@ if user_inp and rep_check and len_check and inv_check and one_check:
 				st.pyplot(use_umaps_Tmisc(composition=ele_list_user_inp,
 										 pD=pD,
 										 n=len(ele_list_user_inp))[1])
+			else:
+				st.write(':red[Currently only works for ternaries. Come back later for higher order visualizations.]')
 
 		if find_reaction_pathway:
 			ax, fig = new_rP(composition=ele_list_user_inp,pD=pD)
